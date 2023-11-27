e9216003ec8de29525a0fae522b694a8be973dc4
Gabriele La Piana <Gabrilp@live.com>
2023-11-27 13:39:28 +0100

    ready for production

--- src/features/countries/countriesAPI.ts
+++ src/features/countries/countriesAPI.ts
@@ -7,11 +7,7 @@ export const fetchCountries = createAsyncThunk(
   async (_, { rejectWithValue }) => {
     try {
       const csvUrl = process.env.NEXT_PUBLIC_COUNTRY_API_URL;
-      console.log("CSV URL:", csvUrl);  // Log the CSV URL
-
       const response = await axios.get(csvUrl!);
-      console.log("Response from API:", response);  // Log the API response
-
       const jsonData = await csvtojson().fromString(response.data);
       return jsonData.map((item: any) => ({
         name: item.Name,
